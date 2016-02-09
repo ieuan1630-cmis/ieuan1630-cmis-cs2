e7897d01471aa26e17c2ed826a6db79896f0e56f
cmis <cmis@cmis-hs14.(none)>
2016-02-09 08:35:43 +0700

    Save functions.py - all functions defined

--- functions.py
+++ functions.py
@@ -4,17 +4,17 @@ from math import sqrt
 
 #define functions:
     #basic maths functions
-def add(x1, x2):
-    return x1 + x2
+def add(x, y):
+    return x + y
 
-def sub(x1, x2):
-    return x1 - x2
+def sub(x, y):
+    return x - y
 
-def mul(x1, x2):
-    return x1 * x2
+def mul(x, y):
+    return x * y
 
-def div(x1, x2):
-    return x1 // x2
+def div(x, y):
+    return x // y
 
     #complex functions
 def secs_to_hrs(secs):
@@ -33,8 +33,14 @@ def tri_area(s1, s2, s3): #s = side length
     s = (s1 + s2 + s3)/2.0
     return sqrt(s * (s - s1) * (s - s2) * (s - s3))
 
-def right_align(w):
-    return " " * (40- (len(w)/2)) + w
+def right_align(txt):
+    return " " * (80 - len(txt)) + txt
+
+def centre(txt):
+    return " " * (40- (len(txt)/2)) + txt
+
+def msg_box(txt):
+    return "+" + ((len(txt) + 4) * "-") + "+" + "\n" + "|  " + txt + "  |" + "\n" + "+" + ((len(txt) + 4) * "-") + "+"
 
 #calculations (r = result):
 r_add = add(3, 4)
@@ -47,6 +53,9 @@ r_sphere_vol = sphere_vol(1.1)
 r_avg_two_vols = avg_two_vols (1, 1.1)
 r_tri_area = tri_area(3, 4, 5)
 r_right_align = right_align("Hello")
+r_centre = centre("Hello")
+r_msg_box1 = msg_box("Hello")
+r_msg_box2 = msg_box("I eat cats!")
 
 #ouput
 print r_add
@@ -59,4 +68,6 @@ print r_sphere_vol
 print r_avg_two_vols
 print r_tri_area
 print r_right_align
-print sqrt(4)
+print r_centre
+print r_msg_box1
+print r_msg_box2
