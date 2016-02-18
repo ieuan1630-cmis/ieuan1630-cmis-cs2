2275e153852c4f14337b403b354638d7491727ac
cmis <cmis@cmis-hs14.(none)>
2016-02-18 07:42:20 +0700

    save str_manip and add random.py; good progress made on msg_box - equation need to be written correctly

---
+++ random.py
@@ -0,0 +1,9 @@
+num_line = 5
+n = 2
+output = "This is a load of sample text... Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation\n ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor\n in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa\n qui officia deserunt mollit anim id est laborum."
+
+print "Hello" + (int(num_line) * "Hello")
+print ((int(n)) *("|  " + output[(74*n):(74*n+74)] + "  |"))
+
+#idea works need right equation; need to set n1 = 0
+
--- str_manip.py
+++ str_manip.py
@@ -4,9 +4,10 @@ def msg_box(txt): #takes text and returns it in a message box
     else:
         #extract \n's right here
         num_line = (len(txt))/(74.0)
-        return "+" + (78 * "-") + "+" + "\n" + (num_line * ("|  " + txt[:73] + "  |") + "\n" + "+" + (78 * "-") + "+"
+	n = num_line
+        return "+" + (78 * "-") + "+" + "\n" + ((int(n)) *("|  " + txt[((1*n) - 1):(74*n)] + "  |")) + "\n" + "+" + (78 * "-") + "+"
 
-output = "This is a load of sample text... Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation\n ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure\n dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa\n qui officia deserunt mollit anim id est laborum."
+output = "This is a load of sample text... Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation\n ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor\n in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa\n qui officia deserunt mollit anim id est laborum."
 
 print msg_box(output)
 
