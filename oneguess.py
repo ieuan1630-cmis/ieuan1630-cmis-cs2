576e73aaa3487c7250bc7ec5354d2d6e7a346cc7
cmis <cmis@cmis-hs14.(none)>
2016-03-15 12:58:39 +0700

    oneguess assignment complete

--- oneguess.py
+++ oneguess.py
@@ -1,32 +1,33 @@
 import random
 
-def set_range(minnum, maxnum):
-    rnge = range(minnum, maxnum)
-    return rnge
-
 def output1(minnum, maxnum):
     return "I'm thinking of a number from {} to {}.".format(minnum, maxnum)
 
 def output2(target, guess, difference):
-    return """
+    print """
 The target was {}.
-Your guess was {}.
-That's under by {}.
-""".format(target, guess, difference)
+Your guess was {}.""".format(target, guess)
+    if difference == 0:
+        return "That's correct! You must be psychic!"
+    elif difference < 0:
+        difference = abs(difference)
+        return "That's over by {}.".format(difference)
+    else: #if reaches here difference should be > 0
+        return "That's under by {}.".format(difference)
 
 def main():
     #input1
     minnum = raw_input("What is the minimum number? ")
     maxnum = raw_input("What is the maximum number? ")
-    #processing1
-    rnge = set_range(int(minnum), int(maxnum))
     #output1
-    print output(minnum, maxnum)
+    print output1(minnum, maxnum)
     #input2
     guess = raw_input("What do you think it is? ")
-    #processing2
+    #processing1
+    target = random.randint(int(minnum),int(maxnum))
+    difference = target - int(guess)
     #output2
-    output2(target, guess, difference)
+    print output2(target, guess, difference)
     
 
 main()
