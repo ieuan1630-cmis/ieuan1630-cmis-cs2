9a610a770beea3b8b334ad3e0aa3f4e06ea7aed0
cmis <cmis@cmis-hs00.(none)>
2016-03-28 10:57:43 +0700

    questions complete, and delayed gratifcation checker, need to do response range and iq calculator

--- conditionals.py
+++ conditionals.py
@@ -59,9 +59,22 @@ def mc_points(result):
     else:
         return 0
 
+def delayed_gratification_check(answer):
+    print "Haha"
+    see_answer = raw_input("Would you like to see the answer?(y or n)")
+    if see_answer == "y":
+        print answer
+        return False
+    elif see_answer == "n":
+        return True
+
+def calculate_IQ(score):
+    age = raw_input("How old are you?")
+    #mental age as calculated by their score divided by their actual age x100 = IQ
 def main():
     score = 0
-    print "This quiz will test your IQ!"
+    print """This quiz will test your IQ!
+Your score along with all the answers will be shown at the end."""
     raw_input("Hit 'enter' to begin") #hitting enter finishes the raw_input part and gets the script to continue running
     raw_input("FREE RESPONSE SECTION:")
     A1 = convert_caps(raw_input("""Q1: Define 'delayed gratification'.
@@ -72,11 +85,13 @@ def main():
         score = score + 1
     else:
         score = score
+    dgc1 = delayed_gratification_check("DELAYED GRATIFICATION REFERS TO THE TRADE OFF BETWEEN THE TEMPTATION OF AN IMMEDIATE REWARD AND A GREATER REWARD IN THE FUTURE")
 
     A2 = convert_caps(raw_input("""Q2: What does IQ stand for?
 """))
     if A2 == "INTELLIGENCE QUOTIENT":
         score = score + 1
+    dgc2 = delayed_gratification_check("INTELLIGENCE QUOTIENT")
 
     A3 = convert_caps(raw_input("""Q3: Does your mother know you're stupid?
 """))
@@ -90,57 +105,89 @@ def main():
         score = score - 1
     else:
         score = score
+    dgc3 = delayed_gratification_check("EVEN A SCORPION IS A GAZELLE IN THE EYES OF ITS MOTHER")
 
-    raw_input("MULTIPLE CHOICE SECTION:")# for multiple choice should make sure responses are in allowed responses
+
+    A4 = convert_caps(raw_input("""Q4: What is the product of 0 and 1 over cosine 0?
+"""))
+    if A4 == "UNDEFINED":
+        score = score + 1
+    dgc4 = delayed_gratification_check("UNDEFINED")
+
+    raw_input("MULTIPLE CHOICE SECTION (type a, b, c):")# for multiple choice should make sure responses are in allowed responses
     A5 = raw_input("""Q5: The month in secret code reads: kbpbdfbq, what month is it?
 a) February b) November c) December
 """)
     R5 = mc_answer_check(A5, "c")
     score = score + mc_points(R5)
+    dgc5 = delayed_gratification_check("c")
 
     A6 = raw_input("""Q6: How many times can you subtract 10 from 100?
 a) ten times b) five times c) one time
 """)
     R6 = mc_answer_check(A6, "c")
     score = score + mc_points(R6)
+    dgc6 = delayed_gratification_check("c")
 
     A7 = raw_input("""Q7: Mary's mother had three children, the first was called April, the second May, what was the name of the third child?
 a) June b) July c) Mary
 """)
     R7 = mc_answer_check(A7, "c")
     score = score + mc_points(R7)
+    dgc7 = delayed_gratification_check("c")
  
     A8 = raw_input("""Q8: If you were running in a race and paced the person in 2nd place what position would you be in?
 a) 1st b) 2nd c) 3rd
 """)
     R8 = mc_answer_check(A8, "b")
     score = score + mc_points(R8)
+    dgc8 = delayed_gratification_check("b")
 
     A9 = raw_input("""Q9: If an electric train is traveling south, which way is the smoke going?
 a)North b) East c) Neither
 """)
     R9 = mc_answer_check(A9, "c")
     score = score + mc_points(R9)
+    dgc9 = delayed_gratification_check("c")
  
     A10 = raw_input("""Q10: Is it legal for a man to marry a widow's sister?
 a)Yes b) In some countries c) He can't
 """)
     R10 = mc_answer_check(A10, "c")
     score = score + mc_points(R10)
+    dgc10 = delayed_gratification_check("c")
 
     A11 = raw_input("""Q11: Which month has 28 days?
 a)All b) February c) February only on non leap years
 """)
     R11 = mc_answer_check(A11, "a")
     score = score + mc_points(R11)
+    dgc11 = delayed_gratification_check("a")
 
     A12 = raw_input("""Q12: Before Mt. Everest was discovered, which was the highest mountain in the world?
 a) Mt. Fuji b) Mt Everest c) Mt. Mckinley
 """)
     R12 = mc_answer_check(A12, "b")
     score = score + mc_points(R12)
-    print score
-
+    dgc12 = delayed_gratification_check("b")
+    dgc_final = dgc1 + dgc2 + dgc3 + dgc4 + dgc5 + dgc6 + dgc7 + dgc8 + dgc9 + dgc10 + dgc11 + dgc12
+    if dgc_final == 12:
+        score = score + 12
+        print """Good job you delayed your gratification! Here are the answers:
+1)DELAYED GRATIFICATION REFERS TO THE TRADE OFF BETWEEN THE TEMPTATION OF AN IMMEDIATE REWARD AND A GREATER REWARD IN THE FUTURE
+2)INTELLIGENCE QUOTIENT
+3)EVEN A SCORPION IS A GAZELLE IN THE EYES OF ITS MOTHER
+4)UNDEFINED
+5)c
+6)c
+7)c
+8)b
+9)c
+10)c
+11)a
+12)b
+"""        
+    print """Your score is {}""".format(score)
 
 main()
 
