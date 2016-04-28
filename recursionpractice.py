d88cedc21317a20b31dbb03ae4a9a7a2733d0e18
cmis <cmis@cmis-hs00.(none)>
2016-04-28 13:38:09 +0700

    power done yay

--- recursionpractice.py
+++ recursionpractice.py
@@ -88,10 +88,13 @@ def power(x, nth, result):
 
 def power(x, n):
     if n == 0:
-        return (0,1)
+        x = x*n + 1
+        return x
     else:
-        x = x*
-        return power(x, n-1)
+        return x * power(x, n-1)
+
+
+print power(2, 2)
 
 
 
