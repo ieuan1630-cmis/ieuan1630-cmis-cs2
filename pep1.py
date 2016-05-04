66ad318f7787181042cffe669c80fc420f115c3f
cmis <cmis@cmis-hs00.(none)>
2016-05-04 13:06:37 +0700

    pep1 checked and correct

--- pep1.py
+++ pep1.py
@@ -1,8 +1,8 @@
 #Project Euler Problem 1
 #If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 #Find the sum of all the multiples of 3 or 5 below 1000.
-
-
+import sys
+sys.setrecursionlimit(1002)
 
 def main(n, summation):
     if n == 0:
@@ -13,4 +13,4 @@ def main(n, summation):
             summation += n
         return main(n, summation)
 
-print main(10, 0)
+print main(1000, 0)
