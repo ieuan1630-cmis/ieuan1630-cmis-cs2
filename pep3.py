a7b2d92b2be4abce448e6173e83deaee62308e87
cmis <cmis@cmis-hs00.(none)>
2016-05-10 10:16:41 +0700

    pep3 efficiently (enough) solved

--- pep3.py
+++ pep3.py
@@ -2,21 +2,22 @@
 #The prime factors of 13195 are 5, 7, 13 and 29.
 #What is the largest prime factor of the number 600851475143 ?
 import sys
+import math
 sys.setrecursionlimit(4000002)
 
-def prime(num, test=2, factors=0):
+def prime(num, test=2):
     while test != num:
         if num % test == 0:
-            factors +=1
             return False
         test += 1
     return True
 
 def largest_prime_factor(num, test=2, highest=1):
-    while test != num:
-        if prime(test) == True: #if test is prime
-            if num % test == 0: #and num is divisible by it
-                highest = test  #make that number the new highest prime factor
+    while highest == 1: #while no new high prime factor has been found
+        if num % test == 0: #if num is divisible by test
+            hi_factor = num/test #num divided by test is a high factor of num
+            if prime(hi_factor) == True: #if high factor is prime
+                highest = hi_factor #it is the highest prime factor
         test += 1
     return highest
 
